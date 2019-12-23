7679df5c39d10b1d3a7e943202b650ce742e0ebc
Luis Rodriguez <lui.rodriguez@gcnewhorizons.net>
2019-12-23 14:14:00 -0400

    Register View Desing add

--- app/src/main/java/com/developer/myappointments/MainActivity.kt
+++ app/src/main/java/com/developer/myappointments/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.developer.myappointments
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvGoToRegister.setOnClickListener{
+            Toast.makeText(this, getString(R.string.please_fill_your_register_data), Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
